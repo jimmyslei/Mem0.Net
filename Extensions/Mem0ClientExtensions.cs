f3454d0d1b1672ea3f38510d1764c17936969ba1
jimmy <787478300@qq.com>
2025-07-10 18:46:04 +0800

    服务注册
    服务注册

---
+++ Extensions/Mem0ClientExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Mem0.Net.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Mem0.Net;
+
+public static class Mem0ClientExtensions
+{
+    public static IServiceCollection AddMem0Client(this IServiceCollection services)
+    {
+        services.AddOptions<Mem0Options>()
+            .BindConfiguration("Mem0");
+
+        services.AddSingleton<IMem0Client, Mem0Client>();
+
+        return services;
+    }
+}
+
